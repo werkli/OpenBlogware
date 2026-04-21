6644af58f1c2f65ada2fe3e941a43d9b1d078dfb
Elias Lackner <lackner.elias@gmail.com>
2026-04-21 19:53:53 +0200

    fix: optimise cms listing integration (#72)
    * fix: optimise cms listing
    
    * fix: compiled Administration scripts
    
    * fix: optimise cms listing
    
    * fix: compiled Administration scripts

--- src/Resources/app/administration/src/module/blog-module/extension/sw-cms/page/sw-cms-list/index.js
+++ src/Resources/app/administration/src/module/blog-module/extension/sw-cms/page/sw-cms-list/index.js
@@ -1,14 +1,107 @@
+import BLOG from '../../../../constant/open-blogware.constant';
+import template from './sw-cms-list.html.twig';
+
+const { Data: { Criteria } } = Shopware;
+
 export default {
+    template,
+
     computed: {
-        sortPageTypes() {
-            const sortPageTypes = this.$super('sortPageTypes');
+        listCriteria() {
+            const criteria = this.$super('listCriteria');
 
-            sortPageTypes.push({
-                value: 'blog_detail',
-                name: this.$tc('sw-cms.sorting.labelSortByBlogPages'),
+            criteria.getAssociation('blogEntries').addSorting(Criteria.sort('title', 'ASC')).setLimit(this.associationLimit);
+
+            if (this.currentPageType === null) {
+                criteria.addFilter(Criteria.not('AND', [
+                    Criteria.equals('type', BLOG.PAGE_TYPES.BLOG_DETAIL),
+                ]));
+            }
+
+            return criteria;
+        },
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    methods: {
+        createdComponent() {
+            if (this.assignablePageTypes.includes('blogEntries')) {
+                return;
+            }
+
+            this.assignablePageTypes.push('blogEntries');
+        },
+
+        onListItemClick(page) {
+            if (!this.isBlogPage(page)) {
+                this.$super('onListItemClick', page);
+
+                return;
+            }
+
+            const blogEntry = this.getLinkedBlogEntry(page);
+
+            if (!blogEntry) {
+                return;
+            }
+
+            this.$router.push({
+                name: 'blog.module.detail',
+                params: { id: blogEntry.id },
             });
+        },
 
-            return sortPageTypes;
+        deleteDisabledToolTip(page) {
+            const disabledToolTip = this.$super('deleteDisabledToolTip', page);
+
+            if (page.type === BLOG.PAGE_TYPES.BLOG_DETAIL) {
+                disabledToolTip.message = this.$tc('sw-cms.general.deleteDisabledBlogToolTip');
+            }
+
+            return disabledToolTip;
+        },
+
+        getPageBlogCount(page) {
+            return page.extensions.blogEntries.length;
+        },
+
+        getPageCount(page) {
+            const originalPageCount = this.$super('getPageCount', page);
+            let pageCount = Number.isInteger(originalPageCount) ? originalPageCount : 0;
+
+            pageCount += this.getPageBlogCount(page);
+
+            return pageCount ? pageCount : originalPageCount;
+        },
+
+        getPages(page) {
+            const pages = this.$super('getPages', page);
+
+            return [
+                ...pages,
+                ...page.extensions.blogEntries.map((item) => item.title),
+            ];
+        },
+
+        optionContextDeleteDisabled(page) {
+            const deleteDisabled = this.$super('optionContextDeleteDisabled', page);
+
+            return deleteDisabled || this.getPageBlogCount(page) > 0;
+        },
+
+        isBlogPage(page) {
+            return page.type === BLOG.PAGE_TYPES.BLOG_DETAIL;
+        },
+
+        getLinkedBlogEntry(page) {
+            if (!this.isBlogPage(page)) {
+                return null;
+            }
+
+            return page.extensions.blogEntries.at(0);
         },
     },
 };
